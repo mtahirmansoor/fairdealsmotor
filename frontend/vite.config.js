3978d7e31a50be1165f621b5d768950f94a5dbbd
mtahirmansoor <132584737+mtahirmansoor@users.noreply.github.com>
2024-11-19 09:40:57 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- 
+  root: 'frontend',  // Set the root directory to 'frontend' where your index.html is
   plugins: [react()],
-    base: './',
-
-});
+  base: './',         // Use './' if deploying to a subfolder
+})
